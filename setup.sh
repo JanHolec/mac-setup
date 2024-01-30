fda77fab358a312bbcde1ee6f6771692cd33fd18
JanHolec <jan.holec66@gmail.com>
2024-01-30 23:32:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,35 +7,24 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/janholec/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install colima
-brew install colima
+brew install \
+  colima \
+  docker \
+  helm \
+  minikube \
+  wget \
+  coreutils \
+  azure-cli
 
-# Install docker cli
-brew install docker
-
-# Install minikube
-brew install minikube
-
-# Install utils
-brew install wget
-
-# Install Helm
-brew install helm
-
-# Install coreutils
-brew install coreutils
+brew install --cask \
+  visual-studio-code \
+  1password \
+  iterm2
 
 # Install docker-compose
 brew install docker-compose
 mkdir -p ~/.docker/cli-plugins
 ln -sfn /opt/homebrew/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
 
-# Install Azure CLI
-brew install azure-cli
-
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-brew install --cask \
-  visual-studio-code
-
-brew install --cask iterm2
